50b3034b2fba04f9a77416508e6e1aa4a2ae93f2
gabrielzferreira <gabrielferreira738292@gmail.com>
2025-12-28 22:40:31 -0400

    Fix: Remove force-static from error pages and set node engine v0.1.0-fix10

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-export const dynamic = "force-static"
+
 
 export default function GlobalError({
     error,
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 
-export const dynamic = "force-static"
+
 
 export default function NotFound() {
     return (
